051ea2a5fdb6438b2031827f9cbcdba958fd869c
Andres-XL <amovilla1103@gmail.com>
2020-04-08 00:20:24 -0500

    black triangle progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import './game';
+import './js/game';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-
     </div>
   );
 }
-
-export default App;
---
+++ src/js/game.js
@@ -0,0 +1,15 @@
+import * as PIXI from 'pixi.js';
+
+const app = new PIXI.Application({
+    width: 256,         // default: 800
+    height: 256,        // default: 600
+    antialias: true,    // default: false
+    transparent: false, // default: false
+    resolution: 1       // default: 1
+});
+document.body.appendChild(app.view);
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
